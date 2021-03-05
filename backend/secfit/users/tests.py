32f43328971ffc0c742a1dd882389674281a4e60
Vegard46 <veg_ander@hotmail.com>
2021-03-05 16:37:46 +0100

    Wrote 1 test for UserSerializer, tried validate_password testing but was unsuccessful

--- backend/secfit/users/tests.py
+++ backend/secfit/users/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from django.contrib.auth import password_validation
+from .serializers import UserSerializer
 
 # Create your tests here.
+
+class UserCreationTestCase(TestCase):
+    userData = {
+                "username": "Thomas42",
+                "email": "thom_as@coldmail.com",
+                "password": "wordpass321",
+                "phone_number": "77755566",
+                "country": "NoMansLand",
+                "city": "Capitalum",
+                "street_address": "221B Baker Street"
+            }
+
+    created_user = None
+
+    def setUp(self):
+        self.created_user = UserSerializer.create(self, self.userData)
+
+    def test_user_creation(self):
+        self.assertEquals(self.userData["username"], self.created_user.username)
+        self.assertEquals(self.userData["phone_number"], self.created_user.phone_number)
+        self.assertEquals(self.userData["city"], self.created_user.city)
+
+# class ValidatePasswordTestCase(TestCase):
+#     userData = {
+#                 "username": "Thomas42",
+#                 "email": "thom_as@coldmail.com",
+#                 "password": "wordpass321",
+#                 "phone_number": "77755566",
+#                 "country": "NoMansLand",
+#                 "city": "Capitalum",
+#                 "street_address": "221B Baker Street"
+#             }
+#
+#     createdUser = None
+#
+#     def setUp(self):
+#         self.createdUser = UserSerializer.create(self, self.userData)
+#
+#     def test_password_validation(self):
+#         self.assertEquals(True, password_validation.validate_password(self.createdUser.password))
+
+
+
+
