52cd63c9a0f7b9109eff3c24428a3dcbaa625ea2
Vegard46 <veg_ander@hotmail.com>
2021-03-07 14:19:08 +0100

    set up mock tests for full statement coverage of workoutPermissions

--- backend/secfit/workouts/tests.py
+++ backend/secfit/workouts/tests.py
@@ -2,7 +2,37 @@
 Tests for the workouts application.
 """
 from django.test import TestCase
+from rest_framework import permissions
+from workouts.models import Workout
 
 # Create your tests here.
 
+
+class WorkoutsPermissionsTestCase(TestCase):
+
+    variable = 1
+
+    def setUp(self):
+        self.variable = 2
+
+    def test_is_owner(self):
+        self.assertEqual(self.variable, 2)
+
+    def test_is_owner_of_workout(self):
+        self.assertEqual(self.variable, 2)
+
+    def test_is_coach_and_visible_to_coach(self):
+        self.assertEqual(self.variable, 2)
+
+    def test_is_coach_of_workout_and_visible_to_coach(self):
+        self.assertEqual(self.variable, 2)
+
+    def test_is_public(self):
+        self.assertEqual(self.variable, 2)
+
+    def test_is_workout_public(self):
+        self.assertEqual(self.variable, 2)
+
+    def test_is_read_only(self):
+        self.assertEqual(self.variable, 2)
 
