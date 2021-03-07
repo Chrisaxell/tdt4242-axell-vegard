0bb2fef3e305284ff53564682c1cabd93c904aa8
Vegard46 <veg_ander@hotmail.com>
2021-03-07 19:59:34 +0100

    Full statement coverage of User-serialization

--- backend/secfit/users/tests.py
+++ backend/secfit/users/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
-from django.contrib.auth import password_validation
 from .serializers import UserSerializer
+from .models import User
 
 # Create your tests here.
 
+
 class UserCreationTestCase(TestCase):
+
+    # Test data for test user
     userData = {
                 "username": "Thomas42",
                 "email": "thom_as@coldmail.com",
@@ -15,18 +18,29 @@ class UserCreationTestCase(TestCase):
                 "street_address": "221B Baker Street"
             }
 
-    created_user = None
-
     def setUp(self):
-        self.created_user = UserSerializer.create(self, self.userData)
 
-    def test_user_creation(self):
-        self.assertEquals(self.userData["username"], self.created_user.username)
-        self.assertEquals(self.userData["phone_number"], self.created_user.phone_number)
-        self.assertEquals(self.userData["city"], self.created_user.city)
+        # Initializing user serializer
+        self.serializer = UserSerializer()
+
+        # Create an entry of a user using the user-serializer
+        self.serializer.create(self.userData)
+
+        # Retrieve the newly created user using its primary key
+        self.saved_user = User.objects.get(pk=1)
+
+    def test_user_creation_and_storage(self):
+
+        # We test that the integrity of the userData is preserved through serialization, saving and retrieval
+        self.assertEquals(self.userData["username"], self.saved_user.username)
+        self.assertEquals(self.userData["phone_number"], self.saved_user.phone_number)
+        self.assertEquals(self.userData["city"], self.saved_user.city)
 
     def test_validate_password(self):
-        self.assertEquals(True, True)
+
+        # We test that the password validation validates the correct password, but rejects a faulty password
+        self.assertEquals(self.serializer.validate_password(self.userData["password"]), self.userData["password"])
+        self.assertNotEqual(self.serializer.validate_password("WrongPassword"), self.userData["password"])
 
 
 
